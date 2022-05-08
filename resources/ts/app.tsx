d27c5a939b44029abefa8a716e9715a1638ac499
nishimurasyota <syota4723@gmail.com>
2022-05-08 17:44:06 +0900

    :sparkles: ReactQueryでAPIからデータ取得を実装

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import {Router} from "./Route"
-
+import { QueryClient , QueryClientProvider } from "react-query"
 
 export const App: React.VFC = () => {
+    const queryClient = new QueryClient({
+        defaultOptions:{
+            queries:{
+                retry:false
+            },
+            mutations:{
+                retry:false
+            }
+        }
+    });
     return (
-        <Router />
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
     )
 }
