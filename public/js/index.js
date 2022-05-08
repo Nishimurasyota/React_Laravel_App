61d57688876707e10a970b6691cf41457daa22a0
nishimurasyota <syota4723@gmail.com>
2022-05-08 22:00:01 +0900

    :sparkles: tasks/indexのコンポーネント分割

--- public/js/index.js
+++ public/js/index.js
@@ -2211,6 +2211,195 @@ exports.Router = Router;
 
 /***/ }),
 
+/***/ "./resources/ts/api/TaskApi.tsx":
+/*!**************************************!*\
+  !*** ./resources/ts/api/TaskApi.tsx ***!
+  \**************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+var __generator = this && this.__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function sent() {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) {
+      try {
+        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [op[0] & 2, t.value];
+
+        switch (op[0]) {
+          case 0:
+          case 1:
+            t = op;
+            break;
+
+          case 4:
+            _.label++;
+            return {
+              value: op[1],
+              done: false
+            };
+
+          case 5:
+            _.label++;
+            y = op[1];
+            op = [0];
+            continue;
+
+          case 7:
+            op = _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+
+          default:
+            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+              _ = 0;
+              continue;
+            }
+
+            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+              _.label = op[1];
+              break;
+            }
+
+            if (op[0] === 6 && _.label < t[1]) {
+              _.label = t[1];
+              t = op;
+              break;
+            }
+
+            if (t && _.label < t[2]) {
+              _.label = t[2];
+
+              _.ops.push(op);
+
+              break;
+            }
+
+            if (t[2]) _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+        }
+
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [6, e];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
+
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.getTasks = void 0;
+
+var axios_1 = __importDefault(__webpack_require__(/*! axios */ "./node_modules/axios/index.js"));
+
+var getTasks = function getTasks() {
+  return __awaiter(void 0, void 0, void 0, function () {
+    var data;
+    return __generator(this, function (_a) {
+      switch (_a.label) {
+        case 0:
+          return [4
+          /*yield*/
+          , axios_1["default"].get("api/tasks")];
+
+        case 1:
+          data = _a.sent().data;
+          return [2
+          /*return*/
+          , data];
+      }
+    });
+  });
+};
+
+exports.getTasks = getTasks;
+
+/***/ }),
+
 /***/ "./resources/ts/app.tsx":
 /*!******************************!*\
   !*** ./resources/ts/app.tsx ***!
@@ -2376,149 +2565,6 @@ exports.LoginPage = LoginPage;
 "use strict";
 
 
-var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
-  function adopt(value) {
-    return value instanceof P ? value : new P(function (resolve) {
-      resolve(value);
-    });
-  }
-
-  return new (P || (P = Promise))(function (resolve, reject) {
-    function fulfilled(value) {
-      try {
-        step(generator.next(value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function rejected(value) {
-      try {
-        step(generator["throw"](value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function step(result) {
-      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-    }
-
-    step((generator = generator.apply(thisArg, _arguments || [])).next());
-  });
-};
-
-var __generator = this && this.__generator || function (thisArg, body) {
-  var _ = {
-    label: 0,
-    sent: function sent() {
-      if (t[0] & 1) throw t[1];
-      return t[1];
-    },
-    trys: [],
-    ops: []
-  },
-      f,
-      y,
-      t,
-      g;
-  return g = {
-    next: verb(0),
-    "throw": verb(1),
-    "return": verb(2)
-  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
-    return this;
-  }), g;
-
-  function verb(n) {
-    return function (v) {
-      return step([n, v]);
-    };
-  }
-
-  function step(op) {
-    if (f) throw new TypeError("Generator is already executing.");
-
-    while (_) {
-      try {
-        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-        if (y = 0, t) op = [op[0] & 2, t.value];
-
-        switch (op[0]) {
-          case 0:
-          case 1:
-            t = op;
-            break;
-
-          case 4:
-            _.label++;
-            return {
-              value: op[1],
-              done: false
-            };
-
-          case 5:
-            _.label++;
-            y = op[1];
-            op = [0];
-            continue;
-
-          case 7:
-            op = _.ops.pop();
-
-            _.trys.pop();
-
-            continue;
-
-          default:
-            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
-              _ = 0;
-              continue;
-            }
-
-            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
-              _.label = op[1];
-              break;
-            }
-
-            if (op[0] === 6 && _.label < t[1]) {
-              _.label = t[1];
-              t = op;
-              break;
-            }
-
-            if (t && _.label < t[2]) {
-              _.label = t[2];
-
-              _.ops.push(op);
-
-              break;
-            }
-
-            if (t[2]) _.ops.pop();
-
-            _.trys.pop();
-
-            continue;
-        }
-
-        op = body.call(thisArg, _);
-      } catch (e) {
-        op = [6, e];
-        y = 0;
-      } finally {
-        f = t = 0;
-      }
-    }
-
-    if (op[0] & 5) throw op[1];
-    return {
-      value: op[0] ? op[1] : void 0,
-      done: true
-    };
-  }
-};
-
 var __importDefault = this && this.__importDefault || function (mod) {
   return mod && mod.__esModule ? mod : {
     "default": mod
@@ -2532,9 +2578,9 @@ exports.TaskPage = void 0;
 
 var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
 
-var axios_1 = __importDefault(__webpack_require__(/*! axios */ "./node_modules/axios/index.js"));
+var taskInput_1 = __webpack_require__(/*! ../tasks/taskInput */ "./resources/ts/pages/tasks/taskInput.tsx");
 
-var react_query_1 = __webpack_require__(/*! react-query */ "./node_modules/react-query/es/index.js");
+var taskList_1 = __webpack_require__(/*! ../tasks/taskList */ "./resources/ts/pages/tasks/taskList.tsx");
 
 var TaskPage = function TaskPage() {
   // const [tasks,setTasks] = useState<Task[]>([]);
@@ -2546,25 +2592,80 @@ var TaskPage = function TaskPage() {
   // useEffect(()=>{
   //     getTasks();
   // })
-  var _a = (0, react_query_1.useQuery)("tasks", function () {
-    return __awaiter(void 0, void 0, void 0, function () {
-      var data;
-      return __generator(this, function (_a) {
-        switch (_a.label) {
-          case 0:
-            return [4
-            /*yield*/
-            , axios_1["default"].get("api/tasks")];
+  return react_1["default"].createElement(react_1["default"].Fragment, null, react_1["default"].createElement(taskInput_1.TaskInput, null), react_1["default"].createElement(taskList_1.TaskList, null));
+};
 
-          case 1:
-            data = _a.sent().data;
-            return [2
-            /*return*/
-            , data];
-        }
-      });
-    });
-  }),
+exports.TaskPage = TaskPage;
+
+/***/ }),
+
+/***/ "./resources/ts/pages/tasks/taskInput.tsx":
+/*!************************************************!*\
+  !*** ./resources/ts/pages/tasks/taskInput.tsx ***!
+  \************************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.TaskInput = void 0;
+
+var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+
+var TaskInput = function TaskInput() {
+  return react_1["default"].createElement("form", {
+    className: "input-form"
+  }, react_1["default"].createElement("div", {
+    className: "inner"
+  }, react_1["default"].createElement("input", {
+    type: "text",
+    className: "input",
+    placeholder: "TODO\u3092\u5165\u529B\u3057\u3066\u304F\u3060\u3055\u3044\u3002",
+    defaultValue: ""
+  }), react_1["default"].createElement("button", {
+    className: "btn is-primary"
+  }, "\u8FFD\u52A0")));
+};
+
+exports.TaskInput = TaskInput;
+
+/***/ }),
+
+/***/ "./resources/ts/pages/tasks/taskList.tsx":
+/*!***********************************************!*\
+  !*** ./resources/ts/pages/tasks/taskList.tsx ***!
+  \***********************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.TaskList = void 0;
+
+var react_1 = __importDefault(__webpack_require__(/*! react */ "./node_modules/react/index.js"));
+
+var TaskQuery_1 = __webpack_require__(/*! ../../queries/TaskQuery */ "./resources/ts/queries/TaskQuery.tsx");
+
+var TaskList = function TaskList() {
+  var _a = (0, TaskQuery_1.UseTasks)(),
       tasks = _a.data,
       status = _a.status;
 
@@ -2582,18 +2683,7 @@ var TaskPage = function TaskPage() {
     }, "\u767B\u9332\u3055\u308C\u305FTodo\u306F\u3042\u308A\u307E\u305B\u3093");
   }
 
-  return react_1["default"].createElement(react_1["default"].Fragment, null, react_1["default"].createElement("form", {
-    className: "input-form"
-  }, react_1["default"].createElement("div", {
-    className: "inner"
-  }, react_1["default"].createElement("input", {
-    type: "text",
-    className: "input",
-    placeholder: "TODO\u3092\u5165\u529B\u3057\u3066\u304F\u3060\u3055\u3044\u3002",
-    defaultValue: ""
-  }), react_1["default"].createElement("button", {
-    className: "btn is-primary"
-  }, "\u8FFD\u52A0"))), react_1["default"].createElement("div", {
+  return react_1["default"].createElement("div", {
     className: "inner"
   }, react_1["default"].createElement("ul", {
     className: "task-list"
@@ -2642,10 +2732,38 @@ var TaskPage = function TaskPage() {
     className: "checkbox-input"
   })), react_1["default"].createElement("div", null, react_1["default"].createElement("span", null, "\u6383\u9664")), react_1["default"].createElement("button", {
     className: "btn is-delete"
-  }, "\u524A\u9664")))));
+  }, "\u524A\u9664"))));
 };
 
-exports.TaskPage = TaskPage;
+exports.TaskList = TaskList;
+
+/***/ }),
+
+/***/ "./resources/ts/queries/TaskQuery.tsx":
+/*!********************************************!*\
+  !*** ./resources/ts/queries/TaskQuery.tsx ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.UseTasks = void 0;
+
+var TaskApi_1 = __webpack_require__(/*! ../api/TaskApi */ "./resources/ts/api/TaskApi.tsx");
+
+var react_query_1 = __webpack_require__(/*! react-query */ "./node_modules/react-query/es/index.js");
+
+var UseTasks = function UseTasks() {
+  return (0, react_query_1.useQuery)("tasks", function () {
+    return (0, TaskApi_1.getTasks)();
+  });
+};
+
+exports.UseTasks = UseTasks;
 
 /***/ }),
 
--- resources/ts/pages/tasks/index.tsx
+++ resources/ts/pages/tasks/index.tsx
@@ -1,10 +1,7 @@
 import React,{useEffect, useState} from "react";
-import axios from "axios";
-import {useQuery} from "react-query";
-import {Task} from "../../types/Task"
-
-
-export const TaskPage: React.VFC = () => {
+import {TaskInput} from "../tasks/taskInput"
+import {TaskList} from "../tasks/taskList"
+export const TaskPage: React.FC = () => {
 
     // const [tasks,setTasks] = useState<Task[]>([]);
 
@@ -18,68 +15,10 @@ export const TaskPage: React.VFC = () => {
     //     getTasks();
     // })
 
-    const {data:tasks, status} = useQuery("tasks", async () => {
-        const {data} = await axios.get<Task[]>("api/tasks")
-        return data;
-    })
-
-    if(status === "loading"){
-        return <div className="loader" />
-    }else if(status === "error"){
-        return <div className="align-center">読み込みに失敗しました</div>
-    }else if(!tasks || tasks.length <= 0){
-        return <div className="align-center">登録されたTodoはありません</div>
-    }
-
     return (
         <>
-        <form className="input-form">
-            <div className="inner">
-                <input type="text" className="input" placeholder="TODOを入力してください。" defaultValue="" />
-                <button className="btn is-primary">追加</button>
-            </div>
-            </form>
-                <div className="inner">
-                <ul className="task-list">
-                    {tasks.map(task => (
-                    <li key={task.id}>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div><span>{task.title}</span></div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                    ))}
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <form><input type="text" className="input" defaultValue="編集中のTODO" /></form>
-                        <button className="btn">更新</button>
-                    </li>
-                    <li className="done">
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div><span>実行したTODO</span></div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div><span>ゴミ捨て</span></div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                    <li>
-                        <label className="checkbox-label">
-                            <input type="checkbox" className="checkbox-input" />
-                        </label>
-                        <div><span>掃除</span></div>
-                        <button className="btn is-delete">削除</button>
-                    </li>
-                </ul>
-            </div>
+            <TaskInput />
+            <TaskList />
         </>
     )
 }
